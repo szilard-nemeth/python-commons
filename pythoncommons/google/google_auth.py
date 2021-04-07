55b6dc08a41002a241787711b87cf7d2fe502832
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-04-08 01:39:16 +0200

    google_auth.py: Query profile info upon authentication

--- pythoncommons/google/google_auth.py
+++ pythoncommons/google/google_auth.py
@@ -12,6 +12,7 @@ from pythoncommons.google.common import ServiceType
 class GoogleApiAuthorizer:
     CREDENTIALS_FILENAME = 'credentials.json'
     TOKEN_FILENAME = 'token.pickle'
+    DEFAULT_SCOPES = ["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"]
     # If modifying these scopes, delete the file token.pickle.
     DEFAULT_WEBSERVER_PORT = 49555
 
@@ -22,11 +23,19 @@ class GoogleApiAuthorizer:
                  token_filename: str = TOKEN_FILENAME,
                  credentials_filename: str = CREDENTIALS_FILENAME):
         self.service_type = service_type
-        if scopes is None:
-            self.scopes = self.service_type.default_scopes
+        self._set_scopes(scopes)
         self.server_port = server_port
         self.token_filename = token_filename
         self.creds_filename = credentials_filename
+
+    def _set_scopes(self, scopes):
+        self.scopes = scopes
+        if self.scopes is None:
+            self.scopes = self.service_type.default_scopes
+
+        # https://stackoverflow.com/a/51643134/1106893
+        os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = "1"
+        self.scopes.extend(self.DEFAULT_SCOPES)
 
     def authorize(self):
         creds = self._load_token()
@@ -53,6 +62,11 @@ class GoogleApiAuthorizer:
         else:
             flow = InstalledAppFlow.from_client_secrets_file(self.creds_filename, self.scopes)
             self.authed_creds = flow.run_local_server(port=self.server_port)
+
+            # TODO Save credentials tied to profile (email address)
+            session = flow.authorized_session()
+            profile_info = session.get('https://www.googleapis.com/userinfo/v2/me').json()
+            print(profile_info)
         # Save the credentials for the next run
         self._write_token()
         return self.authed_creds
