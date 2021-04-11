0c7968afdd5d3d7ae5c2adec27fa3232ed3eaa22
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-04-11 20:01:10 +0200

    FileUtils.find_repo_root_dir_auto: BUGFIX, seach files_to_search files in files, not the other way around

--- pythoncommons/file_utils.py
+++ pythoncommons/file_utils.py
@@ -198,7 +198,7 @@ class FileUtils:
             LOG.debug(f"Listing files in dir {d}")
             files = os.listdir(d)
             LOG.debug(f"Found files in dir '{d}': {files}")
-            return all(FileUtils.is_file(FileUtils.join_path(d, f)) and f in files_to_search for f in files)
+            return all(FileUtils.is_file(FileUtils.join_path(d, f)) and f in files for f in files_to_search)
 
         if not files_to_search:
             files_to_search = ["setup.py", "requirements.txt"]
