9d78f4c8433abd504408e331c265b1a5f92a9c49
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-04-11 15:29:26 +0200

    Fix OsUtils.get_env_value: Make it static

--- pythoncommons/os_utils.py
+++ pythoncommons/os_utils.py
@@ -2,7 +2,8 @@ import os
 
 
 class OsUtils:
-    def get_env_value(self, env_name, default_value=None):
+    @staticmethod
+    def get_env_value(env_name, default_value=None):
         if env_name in os.environ:
             return os.environ[env_name]
         else:
