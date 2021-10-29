7047d1f8e703962950199a4806e26dd75b44dfb5
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-10-29 17:05:30 +0200

    ObjUtils: Add some helper methods to get static fields

--- pythoncommons/object_utils.py
+++ pythoncommons/object_utils.py
@@ -9,3 +9,11 @@ class ObjUtils:
         LOG.info("Printing properties of obj: %s", obj)
         for prop, value in vars(obj).items():
             LOG.info("%s: %s", prop, value)
+
+    @staticmethod
+    def get_static_fields(clazz):
+        return [v for v, m in vars(clazz).items() if not (v.startswith('_') or callable(m))]
+
+    @staticmethod
+    def get_static_fields_with_values(clazz):
+        return {v: getattr(clazz, v) for v, m in vars(clazz).items() if not (v.startswith('_') or callable(m))}
