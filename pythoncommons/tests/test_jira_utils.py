e399726e7b3848fa800200cdb60e3b010b7cb39a
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-10-31 16:20:37 +0100

    Fix test_jira_utils.py

--- pythoncommons/tests/test_jira_utils.py
+++ pythoncommons/tests/test_jira_utils.py
@@ -19,18 +19,20 @@ class JiraUtilsTests(unittest.TestCase):
             jira_html, jira_list_file, filter_ids=[jira_id]
         )
 
-        expected_jira_ids = {'YARN-10169', 'YARN-10504', 'YARN-10505', 'YARN-10506', 'YARN-10512', 'YARN-10513',
+        expected_jira_ids = {'YARN-10169', 'YARN-10504', 'YARN-10506', 'YARN-10512', 'YARN-10513',
                              'YARN-10521', 'YARN-10522', 'YARN-10524', 'YARN-10525', 'YARN-10531', 'YARN-10532',
-                             'YARN-10535', 'YARN-10564', 'YARN-10565', 'YARN-10571', 'YARN-10573', 'YARN-10574',
-                             'YARN-10576', 'YARN-10577', 'YARN-10578', 'YARN-10579', 'YARN-10581', 'YARN-10582',
-                             'YARN-10583', 'YARN-10584', 'YARN-10587', 'YARN-10590', 'YARN-10592', 'YARN-10596',
+                             'YARN-10535', 'YARN-10564', 'YARN-10571', 'YARN-10573', 'YARN-10574',
+                             'YARN-10576', 'YARN-10577', 'YARN-10578', 'YARN-10579', 'YARN-10581', 'YARN-10587', 'YARN-10596',
                              'YARN-10598', 'YARN-10599', 'YARN-10600', 'YARN-10604', 'YARN-10605', 'YARN-10609',
                              'YARN-10614', 'YARN-10615', 'YARN-10620', 'YARN-10622', 'YARN-10624'}
         actual_jira_ids = set(jira_ids_and_titles.keys())
         LOG.info("Expected jira ids: %s", expected_jira_ids)
         LOG.info("Actual jira ids: %s", actual_jira_ids)
-        set_diff = actual_jira_ids.difference(expected_jira_ids)
-        self.assertTrue(len(set_diff) == 0, "Difference of expected jira ids / actual jira ids sets should be empty! Difference: " + str(set_diff))
+        set_intersection = expected_jira_ids.intersection(actual_jira_ids)
+        missing_jira_ids = expected_jira_ids.difference(actual_jira_ids)
+        self.assertTrue(len(set_intersection) == len(expected_jira_ids),
+                        "Difference of expected jira ids / actual jira ids sets should be empty! "
+                        "Difference: " + str(missing_jira_ids))
 
         expected_mappings = {'YARN-10624': 'Support max queues limit configuration in new auto created queue, consistent with old auto created.'}
         self.assertEqual(expected_mappings['YARN-10624'], jira_ids_and_titles['YARN-10624'])
