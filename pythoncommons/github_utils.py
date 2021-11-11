5bcbe3b9db37e73ceae2eb6b15d500194dab383b
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-11-11 17:51:46 +0100

    GithubUtils: Add some logs

--- pythoncommons/github_utils.py
+++ pythoncommons/github_utils.py
@@ -1,18 +1,27 @@
+import logging
 from enum import Enum
 
 from pythoncommons.os_utils import OsUtils
 
+LOG = logging.getLogger(__name__)
 
 class GithubActionsEnvVar(Enum):
     CI_EXECUTION = "CI"
     GITHUB_ACTIONS = "GITHUB_ACTIONS"
     GITHUB_WORKSPACE = "GITHUB_WORKSPACE"
 
+
 class GitHubUtils:
     @staticmethod
     def is_github_ci_execution() -> bool:
-        return True if OsUtils.get_env_value(GithubActionsEnvVar.GITHUB_ACTIONS.value) else False
+        is_github_ci_exec = OsUtils.get_env_value(GithubActionsEnvVar.GITHUB_ACTIONS.value)
+        if is_github_ci_exec:
+            LOG.debug("Identified Github Actions CI execution")
+        return is_github_ci_exec
 
     @staticmethod
     def get_workspace_path() -> str:
-        return OsUtils.get_env_value(GithubActionsEnvVar.GITHUB_WORKSPACE.value)
+        github_ws_path = OsUtils.get_env_value(GithubActionsEnvVar.GITHUB_WORKSPACE.value)
+        if github_ws_path:
+            LOG.debug("Identified Github Actions CI workspace path: %s", github_ws_path)
+        return github_ws_path
