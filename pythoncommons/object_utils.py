3be62aeedd302ed601c6a2a4644c27ba89fa3319
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-12-27 16:55:11 +0100

    Moved CollectionUtils

--- pythoncommons/object_utils.py
+++ pythoncommons/object_utils.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any, List
+from typing import Any, List, Dict
 
 LOG = logging.getLogger(__name__)
 
@@ -41,3 +41,32 @@ class ListUtils:
         """Yield successive n-sized chunks from lst."""
         for i in range(0, len(lst), n):
             yield lst[i : i + n]
+
+
+class CollectionUtils:
+    @staticmethod
+    def sum_len_of_lists_in_dict(d: Dict):
+        return sum([len(lst) for lst in d.values()])
+
+    @staticmethod
+    def partition(pred, iterable):
+        trues = []
+        falses = []
+        for item in iterable:
+            if pred(item):
+                trues.append(item)
+            else:
+                falses.append(item)
+        return trues, falses
+
+    @staticmethod
+    def partition_multi(predicates, iterable) -> List[List[Any]]:
+        lists = []
+        for i in range(len(predicates)):
+            lists.append([])
+
+        for item in iterable:
+            for idx, pred in enumerate(predicates):
+                if pred(item):
+                    lists[idx].append(item)
+        return lists
