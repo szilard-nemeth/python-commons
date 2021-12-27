a568bd3276a344571a6350e2cb5e2602bbeda433
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-12-27 13:54:29 +0100

    GitHubUtils: Added method that returns PR mergeable status

--- pythoncommons/github_utils.py
+++ pythoncommons/github_utils.py
@@ -1,9 +1,18 @@
 import logging
 from enum import Enum
 
+import requests
+
 from pythoncommons.os_utils import OsUtils
 
 LOG = logging.getLogger(__name__)
+GITHUB_PULLS_API = "https://api.github.com/repos/apache/hadoop/pulls/$PR_ID"
+
+
+class GithubPRMergeStatus(Enum):
+    MERGEABLE = "Mergeable"
+    NOT_MERGEABLE = "Not mergeable"
+    UNKNOWN = "Unknown"
 
 
 class GithubActionsEnvVar(Enum):
@@ -27,3 +36,17 @@ class GitHubUtils:
         if github_ws_path:
             LOG.debug("Detected Github Actions CI workspace path: %s", github_ws_path)
         return github_ws_path
+
+    @staticmethod
+    def is_pull_request_mergeable(pr_id: int):
+        pr_json = requests.get(GitHubUtils.get_pull_request_url(pr_id)).json()
+        if "mergeable" in pr_json:
+            if pr_json["mergeable"]:
+                return GithubPRMergeStatus.MERGEABLE
+            else:
+                return GithubPRMergeStatus.NOT_MERGEABLE
+        return GithubPRMergeStatus.UNKNOWN
+
+    @staticmethod
+    def get_pull_request_url(pr_id: int):
+        return GITHUB_PULLS_API.replace("$PR_ID", str(pr_id))
