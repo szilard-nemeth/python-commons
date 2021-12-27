ff6c8f5f7149c40a7ed2a7a9dfe9fce86e362a05
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-12-27 16:59:30 +0100

    Moved EnvironmentValidator

--- pythoncommons/os_utils.py
+++ pythoncommons/os_utils.py
@@ -72,3 +72,12 @@ class OsUtils:
             return split_res[0] + f"{PASSWORD_PREFIX}****** " + " ".join(split_res[1].split(" ")[1:])
         else:
             return full_command
+
+
+class EnvironmentValidator:
+    @staticmethod
+    def validate_env_vars(env_vars, action=sys.exit, action_params=1, logger=LOG):
+        for env_var in env_vars:
+            if not os.getenv(env_var):
+                logger.error("env var '%s' is not defined! Calling action: '%s(%s)'", env_var, action, action_params)
+                action(action_params)
