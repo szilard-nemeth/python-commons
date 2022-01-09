bda08b4f630177ed7b0042832858eca8fd298f40
Szilard Nemeth <szilard.nemeth88@gmail.com>
2022-01-09 21:09:14 +0100

    PyTestUtils: Trying to fix is_pytest_execution
    https://stackoverflow.com/a/58866220/1106893

--- pythoncommons/test_utils.py
+++ pythoncommons/test_utils.py
@@ -1,9 +1,10 @@
+import os
 import sys
 
 
 class PyTestUtils:
     @staticmethod
     def is_pytest_execution():
-        if "pytest" in sys.modules:
+        if "pytest" in sys.modules and "PYTEST_CURRENT_TEST" in os.environ:
             return True
         return False
