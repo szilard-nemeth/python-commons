7a90755af2b46a3a5e1fddbbc7b95ba9e4be0333
Szilard Nemeth <szilard.nemeth88@gmail.com>
2022-01-25 14:28:55 +0100

    Add main method to test scripts

--- pythoncommons/test-scripts/hello_world_simple.py
+++ pythoncommons/test-scripts/hello_world_simple.py
@@ -7,15 +7,21 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 LOG = logging.getLogger("project_utils")
 LOG.setLevel(logging.DEBUG)
 
-LOG = logging.getLogger(__name__)
-LOG.info("hello world")
 
-LOG.info("sys.path: %s", sys.path)
-ProjectUtils.project_root_determine_strategy = ProjectRootDeterminationStrategy.SYS_PATH
-ProjectUtils.FORCE_SITE_PACKAGES_IN_PATH_NAME = False
-basedir = ProjectUtils.get_output_basedir('test')
-logfilename = ProjectUtils.get_default_log_file('test')
-LOG.info("basedir: " + basedir)
-LOG.info("logfilename: " + logfilename)
+def main():
+    LOG = logging.getLogger(__name__)
+    LOG.info("hello world")
 
-ProjectUtils.reset_root_determine_strategy_to_default()
+    LOG.info("sys.path: %s", sys.path)
+    ProjectUtils.project_root_determine_strategy = ProjectRootDeterminationStrategy.SYS_PATH
+    ProjectUtils.FORCE_SITE_PACKAGES_IN_PATH_NAME = False
+    basedir = ProjectUtils.get_output_basedir("test")
+    logfilename = ProjectUtils.get_default_log_file("test")
+    LOG.info("basedir: " + basedir)
+    LOG.info("logfilename: " + logfilename)
+
+    ProjectUtils.reset_root_determine_strategy_to_default()
+
+
+if __name__ == "__main__":
+    main()
--- pythoncommons/test-scripts/test_project.py
+++ pythoncommons/test-scripts/test_project.py
@@ -10,23 +10,28 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 LOG = logging.getLogger("project_utils")
 LOG.setLevel(logging.DEBUG)
 
-LOG = logging.getLogger(__name__)
-ProjectUtils.project_root_determine_strategy = ProjectRootDeterminationStrategy.SYS_PATH
-output_basedir = ProjectUtils.get_output_basedir('testproject')
-output_child_dir = ProjectUtils.get_output_child_dir("branch-comparator")
-logfilename = ProjectUtils.get_default_log_file('test-log')
+
+def main():
+    LOG = logging.getLogger(__name__)
+    ProjectUtils.project_root_determine_strategy = ProjectRootDeterminationStrategy.SYS_PATH
+    output_basedir = ProjectUtils.get_output_basedir("testproject")
+    output_child_dir = ProjectUtils.get_output_child_dir("branch-comparator")
+    logfilename = ProjectUtils.get_default_log_file("test-log")
+
+    LOG.info("Output basedir: " + output_basedir)
+    LOG.info("Output child dir: " + output_child_dir)
+    LOG.info("logfilename: " + logfilename)
+
+    python_global_site = site.getsitepackages()[0]
+    python_user_site = site.USER_SITE
+    LOG.info("sys.path: %s", sys.path)
+    LOG.info("Global site: " + python_global_site)
+    LOG.info("User site: " + python_user_site)
+
+    LOG.info("Creating FakeBranchComparator...")
+    FakeBranchComparator()
+    ProjectUtils.reset_root_determine_strategy_to_default()
 
 
-LOG.info("Output basedir: " + output_basedir)
-LOG.info("Output child dir: " + output_child_dir)
-LOG.info("logfilename: " + logfilename)
-
-python_global_site = site.getsitepackages()[0]
-python_user_site = site.USER_SITE
-LOG.info("sys.path: %s", sys.path)
-LOG.info("Global site: " + python_global_site)
-LOG.info("User site: " + python_user_site)
-
-LOG.info("Creating FakeBranchComparator...")
-FakeBranchComparator()
-ProjectUtils.reset_root_determine_strategy_to_default()
+if __name__ == "__main__":
+    main()
