e3235e9fed63d0a3bf692316f266a14ff62b4989
Szilard Nemeth <szilard.nemeth88@gmail.com>
2022-05-16 08:52:40 +0200

    parser_config_reader.py: Added code to handle generic line parser + made the parsing logic more generic

--- pythoncommons/file_parser/parser_config_reader.py
+++ pythoncommons/file_parser/parser_config_reader.py
@@ -4,7 +4,7 @@ import os
 import re
 from dataclasses import field, dataclass
 from enum import Enum
-from typing import Dict, List, Pattern, Callable
+from typing import Dict, List, Pattern
 
 from dataclasses_json import LetterCase, dataclass_json
 
@@ -13,22 +13,35 @@ from pythoncommons.file_utils import JsonFileUtils
 from pythoncommons.string_utils import auto_str
 
 DEFAULT_PARSER_CONFIG_FILENAME = "parserconfig.json"
+DEFAULT_PARSE_PREFIX_SEPARATOR = ":"
+DEFAULT_ALLOWED_VALUES_SEPARATOR = ","
+GREEDY_FIELD_POSTFIX = "_greedy"
 
 LOG = logging.getLogger(__name__)
 
 
-class FieldType(Enum):
+class FieldParseType(Enum):
     REGEX = "regex"
     LITERAL = "literal"
+    BOOL = "bool"
+    INT = "int"
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
 class ExtractableField:
-    type: FieldType
-    value: str
+    parse_type: FieldParseType
     optional: bool
+    value: str = None
     extract_inner_group: bool = False
+    allowed_values: str or None = field(default=None)
+    parse_prefix: str or None = field(default=None)
+    precedence: int or None = field(default=100)
+    eat_greedy_without_parse_prefix: bool or None = field(default=False)
+
+    def __post_init__(self):
+        if self.allowed_values:
+            self.allowed_values_list = self.allowed_values.split(",")
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
@@ -44,31 +57,79 @@ class ParsedBlockFormat:
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
-class GenericParserSettings:
+class GenericBlockParserSettings:
     parsed_block_format: ParsedBlockFormat
     date_formats: List[str] = field(
         default_factory=list
     )  # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
 
+    @property
+    def fields_proxy(self):
+        return self.parsed_block_format.fields
+
+    @property
+    def variables_proxy(self):
+        return self.parsed_block_format.variables
+
+    @property
+    def mandatory_fields_proxy(self):
+        return self.parsed_block_format.mandatory_fields
+
+    @property
+    def format_string_proxy(self):
+        return self.parsed_block_format.format_string
+
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
-class GenericParserConfig:
-    generic_parser_settings: GenericParserSettings
+class GenericBlockParserConfig:
+    generic_parser_settings: GenericBlockParserSettings
 
     # dynamic properties
     field_positions: List[str] = None
 
 
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class GenericLineParserSettings:
+    fields: Dict[str, ExtractableField] = field(default_factory=dict)
+    variables: Dict[str, str] = field(default_factory=dict)
+    date_formats: List[str] = field(
+        default_factory=list
+    )  # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+
+    @property
+    def fields_proxy(self):
+        return self.fields
+
+    @property
+    def mandatory_fields_proxy(self):
+        return []
+
+    @property
+    def variables_proxy(self):
+        return self.variables
+
+    @property
+    def format_string_proxy(self):
+        return None
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class GenericLineParserConfig:
+    generic_parser_settings: GenericLineParserSettings
+
+
 @auto_str
 class ParserConfigReader:
-    def __init__(self, data, data_class):
+    def __init__(self, data, obj_data_class, config_type):
         self.data = data
-        self.data_class = data_class
-        conf, ext_conf = self._parse()
-        self.config: GenericParserConfig = conf
-        self.extended_config: data_class = ext_conf
-        self.parsed_block_format = self.config.generic_parser_settings.parsed_block_format
+        self.obj_data_class = obj_data_class
+        conf, ext_conf = self._parse(config_type)
+        self.config: config_type = conf
+        self.extended_config: obj_data_class = ext_conf
+        self.generic_parser_settings = self.config.generic_parser_settings
 
         # Post init
         self._validate()
@@ -78,42 +139,48 @@ class ParserConfigReader:
         if not self.config.generic_parser_settings.date_formats:
             raise ValueError("No date format specified!")
 
-        format_string = self.parsed_block_format.format_string
-        actual_field_names = frozenset((self.parsed_block_format.fields.keys()))
-        self.config.field_positions = list(re.findall(ParsedBlockFormat.FIELD_FORMAT, format_string))
-        expected_field_names = set(self.config.field_positions)
+        format_string = self.generic_parser_settings.format_string_proxy
+        actual_field_names = frozenset((self.generic_parser_settings.fields_proxy.keys()))
+        if format_string:
+            self.config.field_positions = list(re.findall(ParsedBlockFormat.FIELD_FORMAT, format_string))
+            expected_field_names = set(self.config.field_positions)
 
-        if not expected_field_names or any([fn == "" for fn in expected_field_names]):
-            raise ValueError(
-                "Expected field names is empty, this is not normal. Value: {}".format(expected_field_names)
-            )
+            if not expected_field_names or any([fn == "" for fn in expected_field_names]):
+                raise ValueError(
+                    "Expected field names is empty, this is not normal. Value: {}".format(expected_field_names)
+                )
 
-        diff = set(self.parsed_block_format.mandatory_fields).difference(actual_field_names)
+            diff = expected_field_names.difference(actual_field_names)
+            if diff:
+                raise ValueError(
+                    "The following fields are not having the field config object {}. "
+                    "Expected field names: {}".format(diff, expected_field_names)
+                )
+        else:
+            self.config.field_positions = list(self.generic_parser_settings.fields_proxy.keys())
+
+        diff = set(self.generic_parser_settings.mandatory_fields_proxy).difference(actual_field_names)
         if diff:
             raise ValueError(
                 "Found unknown field names: {}. Allowed field names: {}".format(
-                    diff, self.parsed_block_format.mandatory_fields
+                    diff, self.generic_parser_settings.mandatory_fields_proxy
                 )
-            )
-
-        diff = expected_field_names.difference(actual_field_names)
-        if diff:
-            raise ValueError(
-                "The following fields are not having the field config object {}. "
-                "Expected field names: {}".format(diff, expected_field_names)
             )
 
         self._check_variables()
         self._validate_date_formats(self.config.generic_parser_settings.date_formats)
         self._validate_regexes()
+        self._validate_alllowed_values()
 
     def _check_variables(self):
-        for field_name, field_object in self.parsed_block_format.fields.items():
+        for field_name, field_object in self.generic_parser_settings.fields_proxy.items():
+            if field_object.parse_type != FieldParseType.REGEX:
+                continue
             vars = re.findall(ParsedBlockFormat.VAR_PATTERN, field_object.value)
             vars_set = set(vars)
             if vars_set:
                 LOG.debug("Find variables in field '%s': '%s'", field_name, field_object.value)
-                available_vars = self.parsed_block_format.variables
+                available_vars = self.generic_parser_settings.variables_proxy
                 diff = set(vars_set).difference(set(available_vars.keys()))
                 if diff:
                     raise ValueError(
@@ -136,9 +203,9 @@ class ParserConfigReader:
         field_object.value = new_value
 
     def _validate_regexes(self):
-        for f in self.parsed_block_format.fields.values():  # type: ExtractableField
+        for f in self.generic_parser_settings.fields_proxy.values():  # type: ExtractableField
             if f.extract_inner_group:
-                if f.type != FieldType.REGEX:
+                if f.type != FieldParseType.REGEX:
                     raise ValueError(
                         "Invalid config. If 'extractInnerGroup' is enabled, field type should be regex. Field object: {}".format(
                             f
@@ -157,8 +224,17 @@ class ParserConfigReader:
             LOG.debug("Formatting current date with format '%s': %s", fmt, DateUtils.now_formatted(fmt))
         # TODO
 
+    def _validate_alllowed_values(self):
+        for field_name, field_object in self.config.generic_parser_settings.fields_proxy.items():
+            if field_object.parse_type == FieldParseType.BOOL and not field_object.allowed_values:
+                raise ValueError(
+                    "Parse type is set to '{}' on field '{}', but allowed values are not specified!".format(
+                        FieldParseType.BOOL.value, field_name
+                    )
+                )
+
     @staticmethod
-    def read_from_file(data_class, dir=None, filename=None):
+    def read_from_file(obj_data_class, config_type, dir=None, filename=None):
         if filename:
             parser_conf_file = filename
         elif dir:
@@ -167,13 +243,13 @@ class ParserConfigReader:
             parser_conf_file = DEFAULT_PARSER_CONFIG_FILENAME
 
         data_dict = JsonFileUtils.load_data_from_json_file(parser_conf_file)
-        return ParserConfigReader(data_dict, data_class)
+        return ParserConfigReader(data_dict, obj_data_class, config_type)
 
-    def _parse(self):
-        generic_parser_config = GenericParserConfig.from_json(json.dumps(self.data))
+    def _parse(self, config_type):
+        generic_parser_config = config_type.from_json(json.dumps(self.data))
         LOG.info("Generic parser config: %s", generic_parser_config)
 
-        extended_parser_config = self.data_class.from_json(json.dumps(self.data))
+        extended_parser_config = self.obj_data_class.from_json(json.dumps(self.data))
         LOG.info("Extended parser config: %s", extended_parser_config)
         return generic_parser_config, extended_parser_config
 
@@ -204,18 +280,37 @@ class ParserConfigReader:
 
 class RegexGenerator:
     @staticmethod
-    def create_final_regex(config: GenericParserConfig):
-        field_objects: Dict[str, ExtractableField] = config.generic_parser_settings.parsed_block_format.fields
+    def get_regexes(field_objects: Dict[str, ExtractableField]):
+        # Order dict by precedence
+        field_objects = {k: v for k, v in sorted(field_objects.items(), key=lambda item: item[1].precedence)}
+
+        regex_dict: Dict[str, str] = {}
+        used_group_names = {}
+        for field_name, field_object in field_objects.items():
+            group_name = field_name.lower()
+            if group_name not in used_group_names:
+                used_group_names[group_name] = True
+                regex_dict[group_name] = RegexGenerator._create_regex(group_name, field_object)
+            else:
+                raise ValueError("Group name is already used in regex: {}".format(group_name))
+            if field_object.eat_greedy_without_parse_prefix:
+                field_key = group_name + GREEDY_FIELD_POSTFIX
+                regex_dict[field_key] = RegexGenerator._create_regex(field_key, field_object, use_parse_prefix=False)
+        return regex_dict
+
+    @staticmethod
+    def create_final_regex(parser_config):
+        field_objects: Dict[str, ExtractableField] = parser_config.generic_parser_settings.fields_proxy
         final_regex = r""
         used_group_names = {}
-        for field_name in config.field_positions:
+        for field_name in parser_config.field_positions:
             field_object = field_objects[field_name]
             group_name = field_name
             if group_name not in used_group_names:
                 used_group_names[group_name] = 1
                 final_regex += RegexGenerator._create_regex(group_name, field_object)
             else:
-                if group_name not in config.generic_parser_settings.parsed_block_format.mandatory_fields:
+                if group_name not in parser_config.generic_parser_settings.mandatory_fields_proxy:
                     used_group_names[group_name] += 1
                     group_name = f"{group_name}_{used_group_names[group_name]}"
                     final_regex += RegexGenerator._create_regex(group_name, field_object)
@@ -225,8 +320,30 @@ class RegexGenerator:
         return final_regex
 
     @staticmethod
-    def _create_regex(group_name, field_object: ExtractableField):
+    def _create_regex(group_name, field_object: ExtractableField, use_parse_prefix=True):
         regex_value = field_object.value
+
+        parse_prefix = ""
+        if use_parse_prefix:
+            parse_prefix = field_object.parse_prefix
+            if not parse_prefix:
+                parse_prefix = ""
+            else:
+                parse_prefix += DEFAULT_PARSE_PREFIX_SEPARATOR
+
+        if (
+            field_object.parse_type == FieldParseType.REGEX
+            and regex_value.startswith('"')
+            and regex_value.endswith('"')
+        ):
+            regex_value = regex_value[1:-1]
+        elif field_object.parse_type == FieldParseType.INT:
+            regex_value = r"\d+"
+        elif field_object.parse_type == FieldParseType.BOOL:
+            regex_value = "|".join(field_object.allowed_values_list)
+
+        regex_value = f"{parse_prefix}{regex_value}"
+
         if field_object.extract_inner_group:
             grouped_regex = RegexGenerator._get_inner_group_grouped_regex(group_name, regex_value)
             if field_object.optional:
